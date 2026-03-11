70a2845fbfc948722e8a343b7531168cfe6e950c
threehuna <ivanoviv660@gmail.com>
2026-03-12 00:38:38 +0300

    Initial Commit

---
+++ ScrollFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollFit
+//
+//  Created by Иван Иванов on 12.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
